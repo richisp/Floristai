8256289125251dabab1b512469c4d4531b527568
richisp <mrrewez@gmail.com>
2022-04-25 21:33:47 +0300

    Check on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     # finds existing user, checks to see if user can be authenticated
-    if user.present? && user.authenticate(params[:password])
+    if user.present? && user.authenticate(params[:password]) && check_ga(user)
       # sets up user.id sessions
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in successfully'
     else
-      flash.now[:alert] = 'Invalid email or password'
+      flash.now[:alert] = 'Invalid credentials'
       render :new
     end
   end
@@ -18,5 +18,13 @@ class SessionsController < ApplicationController
     # deletes user session
     session[:user_id] = nil
     redirect_to root_path, notice: 'Logged Out'
+  end
+
+  private
+
+  def check_ga(user)
+    return true unless user.google_secret_value.present?
+
+    user.google_authentic?(params[:ga_code])
   end
 end
